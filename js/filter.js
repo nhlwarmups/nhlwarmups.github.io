abea65480d75bd926d74403bae3313ef4abe1800
nhlwarmups <nhlwarmups@gmail.com>
2023-04-07 13:21:30 -0700

    added mtl, sea

--- js/filter.js
+++ js/filter.js
@@ -11,26 +11,45 @@ export function sortJerseys(jerseys) {
   const sortValue = document.querySelector("#sort-select").value;
   
   if (sortValue === "team") {
-      // sort by team and then artist
-      jerseys.sort(function (a, b) {
-        if (a.team > b.team) return 1;
-        if (a.team < b.team) return -1;
-        if (a.artist > b.artist) return 1;
-        if (a.artist < b.artist) return -1;
-      });
-    } else if (sortValue === "event") {
-      // sort by event and then artist
-      jerseys.sort(function (a, b) {
-        if (a.event > b.event) return 1;
-        if (a.event < b.event) return -1;
-        if (a.artist > b.artist) return 1;
-        if (a.artist < b.artist) return -1;
-      });
+    // sort by team and then artist
+    jerseys.sort(function (a, b) {
+      if (a.team > b.team) return 1;
+      if (a.team < b.team) return -1;
+      if (a.artist > b.artist) return 1;
+      if (a.artist < b.artist) return -1;
+    });
+  } else if (sortValue === "event") {
+    // sort by event and then artist
+    jerseys.sort(function (a, b) {
+      if (a.event > b.event) return 1;
+      if (a.event < b.event) return -1;
+      if (a.artist > b.artist) return 1;
+      if (a.artist < b.artist) return -1;
+    });
   } else if (sortValue === "day-new") {
-      jerseys.sort((a, b) => (a.date[a.date.length-1] < b.date[b.date.length-1]) ? 1 : -1);
+    jerseys.sort(function (a, b) {
+      if (a.date[a.date.length-1] < b.date[b.date.length-1]) return 1;
+      if (a.date[a.date.length-1] > b.date[b.date.length-1]) return -1;
+      if (a.artist > b.artist) return 1;
+      if (a.artist < b.artist) return -1;
+    });
   } else if (sortValue === "day-old") {
     // if worn multiple years, use first date
-      jerseys.sort((a, b) => (a.date[0] > b.date[0]) ? 1 : -1);
+    jerseys.sort(function (a, b) {
+      if (a.date[0] > b.date[0]) return 1;
+      if (a.date[0] < b.date[0]) return -1;
+      if (a.artist > b.artist) return 1;
+      if (a.artist < b.artist) return -1;
+    });
+  } else if (sortValue === "artist") {
+    // if worn multiple years, use first date
+    jerseys.sort(function (a, b) {
+      if (a.artist > b.artist) return 1;
+      if (a.artist < b.artist) return -1;
+      if (a.event > b.artist) return 1;
+      if (a.artist < b.artist) return -1;
+    });
   }
+
   return jerseys;
 }
